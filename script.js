4af1d168dade5de600cc9da2c09a807547891c07
JonLyn <jonlyn22@gmail.com>
2020-12-18 12:30:34 -0500

    one of my last commits as a Juno student :sob:

--- script.js
+++ script.js
@@ -3,35 +3,20 @@ AOS.init();
 const bars = document.querySelector('.hamburgerBars');
 const dropdown = document.querySelector('.hamburgerLinks');
 const dropdownLink = document.querySelector('.hamburgerLinks a');
+const topBar = document.querySelector('.top');
+const bottomBar = document.querySelector('.bottom');
+const hide = document.querySelector('.middle');
+
 
 bars.onclick = function () {
   dropdown.classList.toggle('open');
+  topBar.classList.toggle('topAngle');
+  topBar.classList.toggle('topStraight');
+  bottomBar.classList.toggle('bottomAngle');
+  hide.classList.toggle('middleHide');
 }
 
 dropdownLink.onclick = function () {
   dropdown.classList.remove('open');
 }
 
-// $('.checkbox').on('click', function () {
-//   console.log('yes');
-//   $('.hamburgerLinks').addClass('open');
-// });
-
-// $('.checkbox').on('click', function () {
-//   $('.hamburgerLinks').removeClass('open');
-// });
-
-// var logos = document.querySelectorAll('path[0]');
-// var length = logos.getTotalLength();
-
-// var path2 = document.querySelector('.path2');
-// var length2 = path2.getTotalLength();
-
-// console.log(length);
-// console.log(length2);
-
-const logo = document.querySelectorAll('path');
-for (let i = 0; i < logo.length; i++) {
-  console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-}
-// console.log(logo);
